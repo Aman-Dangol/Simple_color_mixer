ca0f1188257bbbef71663fcff6fd64746559cf54
Aman Dangol <amandangol423@gmail.com>
2023-10-10 21:27:16 +0545

    color code

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ let result = document.getElementById('result');
 let red = document.getElementById('redOut');
 let blue = document.getElementById('blueOut');
 let green = document.getElementById('greenOut');
+let redString,blueString,greenString;
+
 
 function redChange(value){
         red.value = value;
         // r g b 
         result.style.backgroundColor=`rgb(${value},${blue.value},${green.value})`;
+        result.innerText=`(${red.value},${green.value},${blue.value})`;
 }
 
 function blueChange(value){
     blue.value = value;
     // r g b 
     result.style.backgroundColor=`rgb(${red.value},${green.value},${value})`;
+    result.innerText=`(${red.value},${green.value},${blue.value})`;
 
 }
 
@@ -20,5 +24,6 @@ function greenChange(value){
     green.value = value;
     // r g b
     result.style.backgroundColor=`rgb(${red.value},${value},${blue.value})`;
+    result.innerText=`(${red.value},${green.value},${blue.value})`;
 
 }
