a2a9acde2797268e94cc5d089f73ce9b2a1edf76
Aman Dangol <amandangol423@gmail.com>
2023-12-09 00:19:04 +0545

    fixed color issue

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ let redString,blueString,greenString;
 
 function redChange(value){
         red.value = value;
-        // r g b 
-        result.style.backgroundColor=`rgb(${value},${blue.value},${green.value})`;
-        result.innerText=`(${red.value},${green.value},${blue.value})`;
 }
 
 function blueChange(value){
     blue.value = value;
-    // r g b 
-    result.style.backgroundColor=`rgb(${red.value},${green.value},${value})`;
-    result.innerText=`(${red.value},${green.value},${blue.value})`;
 
 }
 
 function greenChange(value){
     green.value = value;
-    // r g b
-    result.style.backgroundColor=`rgb(${red.value},${value},${blue.value})`;
-    result.innerText=`(${red.value},${green.value},${blue.value})`;
 
 }
+
+function update(){
+    result.style.backgroundColor=`rgb(${red.value},${green.value},${blue.value})`;
+    result.innerText=`(${red.value},${green.value},${blue.value})`;
+    requestAnimationFrame(update)
+
+}
+
+update();
+
